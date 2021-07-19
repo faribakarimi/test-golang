11d22b81593569401e95b08d51828c883028ae42
Fariba Karimi <kfariba.fkarimi@gmail.com>
2021-07-20 00:49:57 +0430

    Add models, migrate tables and register

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -20,17 +23,73 @@ var getConnectionString = func(config Config) string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", config.User, config.Password, config.ServerName, config.DB)
 }
 
-var Connector *gorm.DB
+var connector *gorm.DB
 
 func connect(connectionString string) error {
 	var err error
-	Connector, err = gorm.Open("mysql", connectionString)
+	connector, err = gorm.Open("mysql", connectionString)
 	if err != nil {
 		return err
 	}
 	log.Println("Connection was successful!")
 	return nil
 }
+
+type User struct {
+	ID			int 	`json:"id"`
+	Username	string	`json:"username"`
+	Password	string	`json:"password"`
+	Name		string	`json:"name"`
+	Family		string	`json:"family"`
+	Gender		string	`json:"gender"`
+	Age			int		`json:"age"`
+	Balance		int		`json:"balance"`
+}
+
+type Item struct {
+	ID		int		`json:"id"`
+	Name	string	`json:"name"`
+	Price	int		`json:"price"`
+}
+
+type UserItems struct {
+	ID		int	`json:"id"`
+	UserID	int	`json:"userid"`
+	ItemID	int	`json:"itemid"`
+}
+
+func migrateUser(table *User) {
+	connector.AutoMigrate(&table)
+	log.Println("Table Users migrated.")
+}
+
+func migrateItem(table *Item) {
+	connector.AutoMigrate(&table)
+	log.Println("Table Items migrated.")
+}
+
+func migrateUserItem(table *UserItems) {
+	connector.AutoMigrate(&table)
+	log.Println("Table User Items migrated.")
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var user User
+	json.Unmarshal(reqBody, &user)
+	connector.Create(user)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user.ID)
+}
+
+func login(w http.ResponseWriter, r *http.Request)        {}
+func profile(w http.ResponseWriter, r *http.Request)      {}
+func editProfile(w http.ResponseWriter, r *http.Request)  {}
+func buy(w http.ResponseWriter, r *http.Request)          {}
+func returnItems(w http.ResponseWriter, r *http.Request)  {}
+func balance(w http.ResponseWriter, r *http.Request)      {}
+func getUserItems(w http.ResponseWriter, r *http.Request) {}
 
 func handleRequests() {
 
@@ -44,20 +103,9 @@ func handleRequests() {
 	myRouter.HandleFunc("buy", buy).Methods("POST")
 	myRouter.HandleFunc("balance", balance)
 	myRouter.HandleFunc("userItems", getUserItems)
-	
+
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
-
-func register(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the register!")
-}
-func login(w http.ResponseWriter, r *http.Request) {}
-func profile(w http.ResponseWriter, r *http.Request){}
-func editProfile(w http.ResponseWriter, r *http.Request){}
-func buy(w http.ResponseWriter, r *http.Request){}
-func returnItems(w http.ResponseWriter, r *http.Request){}
-func balance(w http.ResponseWriter, r *http.Request){}
-func getUserItems(w http.ResponseWriter, r *http.Request){}
 
 func main() {
 
@@ -75,6 +123,16 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	migrateUser(&User{})
+	migrateItem(&Item{})
+	migrateUserItem(&UserItems{})
+
+	var item Item
+	item.ID = 1
+	item.Name = "item number 1"
+	item.Price = 500
+	connector.Create(item)
 
 	handleRequests()
 }
